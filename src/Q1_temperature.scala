d5f01053f47222aee8d9dbedd5db981ac29307bd
Farzan <farzanroxz123@gmail.com>
2021-05-12 21:01:30 +0530

    Complete the temperature convertion question

--- src/Q1_temperature.scala
+++ src/Q1_temperature.scala
@@ -1,3 +1,11 @@
+
+
+
 object Q1_temperature extends App {
+    println("\nConvert degree CELSIUS Into FAHRENHEIT\n")
+    val valueInCelsius = 35;
+    println("35 degree CELSIUS in FAHRENHEIT:  " + ( (valueInCelsius * 1.8) + 32.0) )
+
+
 
 }
